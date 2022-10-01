98ea7a3bb3c94e892abf498bcdf68179f7147df2
helehan <helehan@sensetime.com>
2022-10-01 10:45:06 +0800

    create ANN

---
+++ MLP/codes/loss.py
@@ -0,0 +1,55 @@
+from __future__ import division
+import numpy as np
+
+
+class EuclideanLoss(object):
+    def __init__(self, name):
+        self.name = name
+
+    def forward(self, input, target):
+        # TODO START
+        '''Your codes here'''
+        pass
+        # TODO END
+
+    def backward(self, input, target):
+		# TODO START
+        '''Your codes here'''
+        pass
+		# TODO END
+
+
+class SoftmaxCrossEntropyLoss(object):
+    def __init__(self, name):
+        self.name = name
+
+    def forward(self, input, target):
+        # TODO START
+        '''Your codes here'''
+        pass
+        # TODO END
+
+    def backward(self, input, target):
+        # TODO START
+        '''Your codes here'''
+        pass
+        # TODO END
+
+
+class HingeLoss(object):
+    def __init__(self, name, margin=5):
+        self.name = name
+        self.margin = margin
+
+    def forward(self, input, target):
+        # TODO START 
+        '''Your codes here'''
+        pass
+        # TODO END
+
+    def backward(self, input, target):
+        # TODO START
+        '''Your codes here'''
+        pass
+        # TODO END
+
