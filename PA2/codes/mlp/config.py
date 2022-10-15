3f5a11eefb3108b609e37837b745402424b34cb9
helehan <helehan@sensetime.com>
2022-10-15 14:11:41 +0800

    buffer

---
+++ PA2/codes/mlp/config.py
@@ -0,0 +1,3 @@
+input_size=32*32*3
+num_feature1=100
+num_feature2=10
--- PA2/codes/mlp/model.py
+++ PA2/codes/mlp/model.py
@@ -1,9 +1,103 @@
+# # -*- coding: utf-8 -*-
+
+# from importlib.metadata import requires
+# from socketserver import ThreadingMixIn
+# import torch
+# from torch import nn
+# from torch.nn import init
+# from torch.nn.parameter import Parameter
+# import config
+# class BatchNorm1d(nn.Module):
+# 	# TODO START
+# 	def __init__(self, num_features,eps=1e-5,mome1=0.8,mome2=0.9):
+# 		super(BatchNorm1d, self).__init__()
+# 		self.num_features = num_features
+# 		self.mome1=mome1
+# 		self.mome2=mome2
+# 		self.eps=eps
+
+# 		# Parameters
+# 		self.weight = nn.Parameter(torch.ones(num_features))
+# 		self.bias =nn.Parameter(torch.ones(num_features))
+
+# 		# Store the average mean and variance
+# 		self.register_buffer('running_mean', torch.ones(num_features))
+# 		self.register_buffer('running_var', torch.ones(num_features))
+
+# 		# Initialize your parameter
+
+# 	def get_mean_and_var(self,inputs):
+# 		return inputs.mean(dim=0),inputs.var(dim=0)
+
+# 	def forward(self, input):
+# 		# input: [batch_size, num_feature_map * height * width]
+# 		training_mode=self.training
+
+# 		if training_mode:
+# 			mean_value,var_value=self.get_mean_and_var(input)
+# 			self.running_mean=self.running_mean*self.mome1+(1-self.mome1)*mean_value
+# 			self.running_var=self.running_var*self.mome2+(1-self.mome2)*var_value
+# 			return (input-mean_value)/torch.sqrt(self.eps+var_value)*self.weight+self.bias
+
+# 		else:
+# 			return (input-self.running_mean)/torch.sqrt(self.eps+self.running_var)*self.weight+self.bias
+
+# 	# TODO END
+
+# class Dropout(nn.Module):
+# 	# TODO START
+# 	def __init__(self, p=0.5):
+# 		super(Dropout, self).__init__()
+# 		self.p = p
+
+# 	def forward(self, input):
+# 		# input: [batch_size, num_feature_map * height * width]
+# 		training_mode=self.training
+# 		if not training_mode:
+# 			return input
+# 		else:
+# 			dp=torch.bernoulli(torch.ones_like(input)*(1-self.p))
+# 			return dp*input/(1-self.p)
+# 	# TODO END
+
+# class Model(nn.Module):
+# 	def __init__(self, drop_rate=0.5):
+# 		super(Model, self).__init__()
+# 		# TODO START
+# 		# Define your layers here
+# 		# TODO END
+
+# 		self.model_list=nn.ModuleList([
+# 			nn.Linear(config.input_size,config.num_feature1),
+# 			BatchNorm1d(config.num_feature1),
+# 			nn.ReLU(),
+# 			Dropout(drop_rate),
+# 			nn.Linear(config.num_feature1,config.num_feature2),
+# 		])
+# 		self.loss = nn.CrossEntropyLoss()
+
+# 	def forward(self, x, y=None):
+# 		# TODO START
+# 		# the 10-class prediction output is named as "logits"
+# 		for sub_module in self.model_list:
+# 			x=sub_module(x)
+# 		logits = x
+# 		# TODO END
+
+# 		pred = torch.argmax(logits, 1)  # Calculate the prediction result
+# 		if y is None:
+# 			return pred
+# 		loss = self.loss(logits, y.long())
+# 		correct_pred = (pred.int() == y.int())
+# 		acc = torch.mean(correct_pred.float())  # Calculate the accuracy in this mini-batch
+
+# 		return loss, acc
+
 # -*- coding: utf-8 -*-
 
 from locale import normalize
 from statistics import variance
 from numpy import average
-from sympy import re
 import torch
 from torch import nn
 from torch.nn import init
@@ -68,7 +162,7 @@ class Dropout(nn.Module):
 		# input: [batch_size, num_feature_map * height * width]
 		if self.training:
 			#! 这里和 Lafite 实现也不太一样
-			dropout_distribution = torch.bernoulli(torch.ones(input.shape) * (1 - self.p))
+			dropout_distribution = torch.bernoulli(torch.ones_like(input) * (1 - self.p))
 			return input * dropout_distribution / (1 - self.p)
 		else:
 			return input
