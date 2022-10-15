fbd3635dfd285115b08c92ae22419f2e1fb9af8a
helehan <helehan@sensetime.com>
2022-10-15 18:00:36 +0800

    fix

--- PA2/codes/cnn/pipeline.py
+++ PA2/codes/cnn/pipeline.py
@@ -2,8 +2,8 @@ from itertools import product
 import subprocess
 
 def get_all_combinations():
-    batch_sizes = [512, 1024, 2048, 4096]
-    learning_rates = [0.001, 0.0001]
+    batch_sizes = [128, 256, 512, 1024]
+    learning_rates = [0.001, 0.0005, 0.0001]
     drop_rates = [0, 0.2, 0.4, 0.6, 0.8]
     return product(batch_sizes, learning_rates, drop_rates)
 
