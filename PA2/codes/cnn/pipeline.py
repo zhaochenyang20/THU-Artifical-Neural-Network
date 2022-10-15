54bdc977f8034dee5e6cefe58b0fe31ee1c52ae2
zhaochen20 <zhaochenyang20@gmail.com>
2022-10-15 17:46:48 +0800

    add pipeline

---
+++ PA2/codes/cnn/pipeline.py
@@ -0,0 +1,15 @@
+from itertools import product
+import subprocess
+
+def get_all_combinations():
+    batch_sizes = [512, 1024, 2048, 4096]
+    learning_rates = [0.001, 0.0001]
+    drop_rates = [0.1, 0.3, 0.5, 0,7, 0.9]
+    return product(batch_sizes, learning_rates, drop_rates)
+
+def pipeline():
+    for batch_size, learning_rate, drop_rate in get_all_combinations():
+        subprocess.run(f"python3 main.py --batch_size {batch_size} --learning_rate {learning_rate} --drop_rate {drop_rate}", shell=True)
+
+if __name__ == "__main__":
+    pipeline()
