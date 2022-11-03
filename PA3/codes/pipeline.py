7f12deb50810fd51409e38862c5fbce46793d8b7
helehan <helehan@sensetime.com>
2022-11-03 20:38:00 +0800

    fix pipeline

--- PA3/codes/pipeline.py
+++ PA3/codes/pipeline.py
@@ -22,8 +22,8 @@ def train_models():
             f"python main.py --pretrain_dir={pretrained_ckpt} --batch_size={batch_size} --using_wandb"
         )
 def train_extraction():
-    experiments = [1 , 2, 3]
-    batch_size = 230
+    experiments = [1, 2, 3]
+    batch_size = 128
     for experiment in experiments:
         print(
             f"python main.py --pretrain_dir=./ckpt/full.tar --extract_layer={experiment} --batch_size={batch_size} --using_wandb"
